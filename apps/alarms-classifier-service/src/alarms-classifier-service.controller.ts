c5de5bdd8810b824c47165f28d4edfba362fc630
Conrado Buhrer <conrado@buhrer.net>
2024-11-27 15:57:32 -0300

    Alarm Classifier and Notifications Services

---
+++ apps/alarms-classifier-service/src/alarms-classifier-service.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Logger } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+
+@Controller()
+export class AlarmsClassifierServiceController {
+  private readonly logger = new Logger(AlarmsClassifierServiceController.name);
+
+  @MessagePattern('alarm.classify')
+  classifyAlarm(@Payload() data: unknown) {
+    this.logger.debug(`Received alarm to classify: ${JSON.stringify(data)}`);
+
+    return {
+      category: ['critical', 'non-critical', 'invalid'][
+        Math.floor(Math.random() * 3)
+      ],
+    };
+  }
+}
--- apps/alarms-service/src/alarms-service.controller.ts
+++ apps/alarms-service/src/alarms-service.controller.ts
@@ -1,14 +1,34 @@
-import { Controller, Logger } from '@nestjs/common';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import { Controller, Inject, Logger } from '@nestjs/common';
+import { ClientProxy, EventPattern, Payload } from '@nestjs/microservices';
+import { MESSAGE_BROKER } from './constants';
+import { lastValueFrom } from 'rxjs';
 
 @Controller()
 export class AlarmsServiceController {
   private readonly logger = new Logger(AlarmsServiceController.name);
 
+  constructor(
+    @Inject(MESSAGE_BROKER) private readonly messageBroker: ClientProxy,
+  ) {}
+
   @EventPattern('alarm.created')
-  create(@Payload() data: unknown) {
+  async create(@Payload() data: { name: string; buildingId: number }) {
     this.logger.debug(
       `Received new "alarm.created" event: ${JSON.stringify(data)}`,
     );
+
+    const alarmClassification = await lastValueFrom(
+      this.messageBroker.send('alarms.classify', data),
+    );
+    this.logger.debug(
+      `Alarm "${data.name}" classified as: ${JSON.stringify(alarmClassification)}`,
+    );
+
+    const notify$ = this.messageBroker.emit('notifications.send', {
+      alarm: data,
+      category: alarmClassification.category,
+    });
+    await lastValueFrom(notify$);
+    this.logger.debug(`Dispatched "notification.sent" event`);
   }
 }
